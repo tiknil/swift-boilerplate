bf8051cf58cc79071f2f1b9c0016e8939a71a289
Tribbia Riccardo <tribbia.riccardo@tiknil.com>
2021-02-10 16:20:25 +0100

    - Corretto nome file di partenza degli unit-test - Prima versione completa script

--- gen.rb
+++ gen.rb
@@ -9,6 +9,7 @@ $version = "0.1.0"
 $year = '2021'
 $debug = false
 $tool_name = 'Swift Project Generator'
+$boilerplate_project_name = 'Boilerplate'
 
 if ARGV.length == 0 || ARGV[0] == 'help'
   puts "#{$tool_name} \nby Tiknil © #{$year} - Version " + $version + "\n\n"
@@ -51,10 +52,90 @@ class SwiftProjectGenerator < Thor
   	Utils.header
   	verbose = options[:verbose]
   	project_name = options[:name]
+  	if project_name == nil || project_name == "" || project_name == "name" 
+  		abort "Nome progetto non valido"
+  	end
+  	project_name = project_name.gsub(" ","")
+  	project_name = project_name.capitalize
 
-  	Utils.print("Generazione del nuovo progetto swift #{project_name}", verbose)
+  	parent = File.expand_path("..", Dir.pwd)
+  	dest_dir = parent + "/" + project_name
 
+  	continue_to_create = options[:force]
+  	if !continue_to_create 
+  		puts "Vuoi avviare la generazione del nuovo progetto swift " + project_name.blue + " nella cartella " + dest_dir.red + "? (Y/n)"
+			confirm = STDIN.gets.chomp
+			if confirm == 'Y'
+				continue_to_create = true
+			end
+		end
+		if !continue_to_create
+			abort "Alla prossima 👋🏻"
+		else
+			time = Benchmark.measure {
+				puts "Avvio generazione nuovo progetto swift " + project_name.blue
+
+				# 1. Creo la cartella dove mettere il nuovo progetto
+				if File.exists? dest_dir 
+					continue_to_ovveride = options[:force]
+					if !continue_to_ovveride 
+						puts "Esiste gia' una cartella con il nome del progetto, la vuoi sovrascrivere? (Y/n)"
+						confirm = STDIN.gets.chomp
+						if confirm == "Y"
+							continue_to_ovveride = true
+						end
+					end
+					if continue_to_ovveride
+						FileUtils.rm_rf dest_dir
+						FileUtils.mkdir dest_dir
+					else
+						abort "Alla prossima 👋🏻"
+					end
+  			else
+  				FileUtils.mkdir dest_dir
+  			end
+
+				# 2. Copio tutti i file 
+				FileUtils.cp_r $boilerplate_project_name , dest_dir, :verbose => true
+				FileUtils.cp_r $boilerplate_project_name + "Tests", dest_dir, :verbose => true
+				FileUtils.cp_r $boilerplate_project_name + "UITests", dest_dir, :verbose => true
+				FileUtils.cp "Podfile", dest_dir, :verbose => true
+				FileUtils.cp "project.yml", dest_dir, :verbose => true
+				
+				#3. Sostituisco ovunque "Boilerplate" con project_name
+				replace_file_name_and_text_in_folder dest_dir, $boilerplate_project_name, project_name, verbose
+			}
+
+			puts "\n"
+			puts "Creazione nuovo progetto " + project_name.blue + " completata, benchmark #{time.real}"
+		end
   end
+
+  no_commands {
+
+  	# Sostituisce nei nomi dei file e nel testo dei file il nome del progetto boilerplate con il nome del nuovo progetto
+  	# @param dir cartella da gestire
+  	# @param nome del progetto boilerplate
+  	# @param nome del nuovo progetto 
+  	def replace_file_name_and_text_in_folder dir, boilerplate_name, project_name, verbose
+  		puts "dir: #{dir}, boilerplate_name #{boilerplate_name}, project_name #{project_name}"
+
+  		Dir.chdir dir do 
+  			Dir.glob("*").each do |file_name|
+  				if File.directory? file_name
+  					replace_file_name_and_text_in_folder("#{dir}/#{file_name}", boilerplate_name, project_name, verbose)
+  				else
+  					base_command = "sed -i '' 's/" + boilerplate_name + "/" + project_name + "/g'"
+  					Utils.execute("#{base_command} #{file_name}", verbose, true, "Sostituzione #{boilerplate_name} nel file #{file_name}")
+  				end
+  				if file_name.include? boilerplate_name
+  					newFolder = file_name.gsub(boilerplate_name, project_name)
+  					FileUtils.mv(file_name, newFolder)
+  				end
+  			end
+  		end
+  	end
+  }
 
 end
 
