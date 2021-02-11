49e5d402eb810e38e538514b75cc3c59d9dd3961
Tribbia Riccardo <tribbia.riccardo@tiknil.com>
2021-02-11 17:18:18 +0100

    - Aggiunto bridgingheader di default

---
+++ Boilerplate/Application/BridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  BridgingHeader.h
+//
+//  Created by Riccardo Tribbia on 11/02/21.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+//#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* BridgingHeader_h */
